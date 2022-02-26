9e5f040d170dffbf9a8d461497c25e0fe5228baf
Huy <buyhuynh123@gmail.com>
2022-02-26 19:35:54 +0700

    sd1

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,43 @@
+import "./Style.css";
+import React from "react";
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink,
+} from "./NavbarElements";
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavLink to="/">
+          <h3 class="animate-charcter"> Home</h3>
+        </NavLink>
+        <Bars />
+        <NavMenu>
+          <NavLink to="/about" activeStyle>
+            About
+          </NavLink>
+          <NavLink to="/services" activeStyle>
+            Services
+          </NavLink>
+          <NavLink to="/contact-us" activeStyle>
+            Contact Us
+          </NavLink>
+          <NavLink to="/sign-up" activeStyle>
+            Sign Up
+          </NavLink>
+
+          <NavLink to="/sign-in" activeStyle>
+            Sign In
+          </NavLink>
+        </NavMenu>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
